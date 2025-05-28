8a05f8a39d8d9a77336fc20ac39a3d11e8b4f45a
VICTOR ADRIAN MORENO GALLARDO <112977@alumnouninter.mx>
2025-05-27 18:58:56 -0600

    a

---
+++ CRUDWINFORMSVIC/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDWINFORMSVIC
+{
+    internal class Auto
+    {
+    internal class Auto : Vehiculo
+    {
+        public Auto(int id, string marca, string modelo, int anio, string color,double precio, string estado)
+        :base(id,marca,modelo,anio,color)
+        {
+            Precio = precio;
+            Estado = estado;
+        }
+
+        public Auto() { }
+
+        public double Precio { get; set; }
+        public string Estado { get; set; }
+    }
+    }
+}
